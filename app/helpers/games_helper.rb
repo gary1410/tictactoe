fa26d85b7c55c2b872b3fcd4060fdda417cd1a56
Gary Tsai <gary.m.tsai@gmail.com>
2015-05-06 15:14:51 -0700

    refactors case statements in to a method on it's own

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -10,42 +10,55 @@ module GamesHelper
   end
 
   def create_cell_class(row, column)
-
-    case row
-
-    when 0
-
-      case column
-      when 0
-        'top left'
-      when 1
-        "top vertical-mid"
-      when 2
-        "top right"
-      end
-
-    when 1
-
-      case column
-      when 0
-        "left horizontal-mid"
-      when 1
-        "vertical-mid horizontal-mid"
-      when 2
-        "right horizontal-mid"
-      end
-
-    when 2
-
-      case column
-      when 0
-        "left bottom"
-      when 1
-        "bottom vertical-mid"
-      when 2
-        "bottom right"
-      end
-
+    row_number = case row
+      when 0 then check_row_0(row, column)
+      when 1 then check_row_1(row, column)
+      when 2 then check_row_2(row, column)
     end
   end
+
+  private
+
+  def check_row_0(row, column)
+    case row
+      when 0
+        case column
+        when 0
+          'top left'
+        when 1
+          "top vertical-mid"
+        when 2
+          "top right"
+        end
+    end
+  end
+
+  def check_row_1(row, column)
+     case row
+      when 1
+        case column
+        when 0
+          "left horizontal-mid"
+        when 1
+          "vertical-mid horizontal-mid"
+        when 2
+          "right horizontal-mid"
+        end
+      end
+  end
+
+  def check_row_2(row, column)
+    case row
+     when 2
+      case column
+        when 0
+          "left bottom"
+        when 1
+          "bottom vertical-mid"
+        when 2
+          "bottom right"
+        end
+      end
+  end
+
 end
